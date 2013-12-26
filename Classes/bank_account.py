93adcfe3723744bd95ea7cf2af3e5500dd08e424
Korben Carreno <korbenc@gmail.com>
2013-12-26 01:15:32 -0500

    added checking_account and savings_account to inherit from base class Account

--- Classes/bank_account.py
+++ Classes/bank_account.py
@@ -8,29 +8,30 @@ class Account(object):
 	"""
 	def __init__(self, init_balance=0):
 		self.balance = init_balance
-		
+	
 	def deposit(self, amount):
 		self.balance += amount
-	
+
 	def withdraw(self, amount):
 		self.balance -= amount
-		
+
 	def overdrawn(self):
 		return self.balance < 0
 	
-"""
-class CheckingAccount:
-	def __init__(self):
-		
-class SavingsAccount:
-	def __init__(self):
-		
-	
-class BusinessAccount:
-	def __init__(self):
-"""		
+class checking_Account(Account):
+	def __init__(self,checking_init=0):
+		self.balance = checking_init
 
-	
-my_account = Account(15)
+class savings_Account(Account):
+	def __init__(self, savings_init=0):
+		self.balance = savings_init
+
+my_account = Account(20)
+my_checking_account = checking_Account(15)
+my_savings_account = savings_Account(25)
 my_account.withdraw(5)
-print my_account.balance
+my_checking_account.withdraw(10)
+my_savings_account.deposit(1)
+print 'checking: %d' %my_checking_account.balance
+print 'Account: %d' %my_account.balance
+print 'Savings: %d' %my_savings_account.balance
